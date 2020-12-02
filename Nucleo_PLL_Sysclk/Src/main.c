2662d796fdc94b02521447e576312623aef8560e
Gustavo Monardez <gustavomonardez@hotmail.com>
2020-12-02 09:33:48 -0800

    PLL Sysclk initial creation

---
+++ Nucleo_PLL_Sysclk/Src/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 25, 2020
+ *      Author: Gustavo
+ */
+
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+#include <string.h>
+
+
+#define FALSE 	0
+#define TRUE 	1
+
+void UART2_Init(void);
+void Error_handler(void);
+
+UART_HandleTypeDef huart2;
+
+
+int main(void) {
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	HAL_Init();
+	UART2_Init();
+
+	while(1);
+
+	return 0;
+}
+
+// High level peripheral initialization
+void UART2_Init(void) {
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	//huart2.Init.OverSampling = UART_OVERSAMPLING_8; not configured
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_handler();
+	}
+}
+
+void Error_handler(void) {
+	while(1);
+}
