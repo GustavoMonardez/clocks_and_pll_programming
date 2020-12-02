2226781cca110f93174cef14afb210de60f8f865
Gustavo Monardez <gustavomonardez@hotmail.com>
2020-12-02 09:27:28 -0800

    HSE clock programming

--- Nucleo_HSE_Sysclk_8Mhz/Src/main.c
+++ Nucleo_HSE_Sysclk_8Mhz/Src/main.c
@@ -10,6 +10,7 @@
 #include "main.h"
 #include <string.h>
 
+
 #define FALSE 	0
 #define TRUE 	1
 
@@ -22,17 +23,69 @@ UART_HandleTypeDef huart2;
 int main(void) {
 	RCC_OscInitTypeDef osc_init;
 	RCC_ClkInitTypeDef clk_init;
+	char msg[100];
 
 	HAL_Init();
 	UART2_Init();
-	// TODO: this project is configured for discovery board and the
-	// settings are meant for nucleo board. Consider switching boards.
+
+	/***************************************************************/
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "SYSCLK :%ldHz\r\n", HAL_RCC_GetSysClockFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "HCLK :%ldHz\r\n", HAL_RCC_GetHCLKFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK1 :%ldHz\r\n", HAL_RCC_GetPCLK1Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+	/***************************************************************/
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK2 :%ldHz\r\n", HAL_RCC_GetPCLK2Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
 	memset(&osc_init, 0, sizeof(osc_init));
 	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
 	osc_init.HSEState = RCC_HSE_BYPASS;
 	if (HAL_RCC_OscConfig(&osc_init) != HAL_OK) {
 		Error_handler();
 	}
+
+	clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+						 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+	clk_init.AHBCLKDivider = RCC_SYSCLK_DIV2;
+	clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+	clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+	if (HAL_RCC_ClockConfig(&clk_init, FLASH_LATENCY_0) != HAL_OK) {
+		Error_handler();
+	}
+
+	__HAL_RCC_HSI_DISABLE();
+
+	// redoing systick configuration
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	UART2_Init();
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "SYSCLK :%ldHz\r\n", HAL_RCC_GetSysClockFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "HCLK :%ldHz\r\n", HAL_RCC_GetHCLKFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK1 :%ldHz\r\n", HAL_RCC_GetPCLK1Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK2 :%ldHz\r\n", HAL_RCC_GetPCLK2Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
 
 	while(1);
 
